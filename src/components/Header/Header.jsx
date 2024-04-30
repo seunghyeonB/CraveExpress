f4532495fc3df71fd08d9feead08fc974df52900
seunghyeon ban <sban6@my.centennialcollege.ca>
2024-04-29 22:45:52 -0400

    craveexpress comit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Order your favourite food here</h2>
+                <p>CraveExpress: Your ultimate solution for convenient and speedy food delivery. Explore a diverse range of local favorites and international delights—all at your fingertips!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
